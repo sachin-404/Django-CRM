01c58e3fa8283d11491be9d967602ef9558b2191
Sachin Kant <sachinkant404@gmail.com>
2023-05-21 01:39:31 +0530

    Update settings.py

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'dcrm.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'dcrmdatabase',
+#         'USER': 'root',
+#         'PASSWORD': 'sql@851978',
+#         'HOST': 'localhost',
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dcrmdatabase',
-        'USER': 'root',
-        'PASSWORD': 'sql@851978',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
